3c2596d768ce39a9384b44e3025116d2293438e5
VLR03 <vitorlr03@gmail.com>
2023-11-22 20:32:42 -0300

    Parte seis completa - Declaração IF

--- shell.py
+++ shell.py
@@ -4,7 +4,5 @@ while True:
     text = input('basic > ')
     result, error = basic.run('<stdin>', text)
 
-    if error:
-        print(error.as_string())
-    else:
-        print(result)
+    if error: print(error.as_string())
+    elif result: print(result)
